038650c051609e5b2675596ac3618ba55cee9291
Nathan Hentges <nathanjhentges@gmail.com>
2021-04-01 20:25:06 -0500

    made inquirer prompt array from questions

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const genMark = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-  "What is your project title?",
-  "What is the description?",
-  "What are the installation instructions?",
-  "What is the usage information?",
-  "What are the contribution guidelines?",
-  "What are the usage guidelines?",
-  "How can users contribute?",
-  "What are the test instructions?",
-  "What is the license type?",
-  "What is your GitHub username?",
-  "What is your email address?"
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description?"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage information?"
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "What are the contribution guidelines?"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What are the test instructions?"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What is the license type?",
+    choices: ["MIT", "GNU GPLv3", "Apache License 2.0"],
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
 
 // TODO: Create a function to initialize app
