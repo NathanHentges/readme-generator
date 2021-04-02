046b1f6a377d196440957545a724d027d40a688b
Nathan Hentges <nathanjhentges@gmail.com>
2021-04-01 21:16:41 -0500

    makes license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,13 +6,13 @@ function renderLicenseBadge(license) {
   }
   switch (license) {
     case "MIT":
-      break;
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
 
     case "GNU GPLv3":
-      break;
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
 
     case "Apache License 2.0":
-      break;
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
 
     default:
       break;
