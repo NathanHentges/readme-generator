5ecd6db828414b65e5cc5adb63dee32198857566
Nathan Hentges <nathanjhentges@gmail.com>
2021-04-01 20:03:47 -0500

    added questions to ask

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is your project title?",
+  "What is the description?",
+  "What are the installation instructions?",
+  "What is the usage information?",
+  "What are the contribution guidelines?",
+  "What are the usage guidelines?",
+  "How can users contribute?",
+  "What are the test instructions?",
+  "What is the license type?",
+  "What is your GitHub username?",
+  "What is your email address?"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
